1cc4dfd871719e976fb22bf2553e02e24f26b8cf
Nilmini Kiriwandala <kiriwandalan@yahoo.com>
2015-06-30 18:56:48 +0530

    Revert "Revert "sdgsdg""
    This reverts commit e79f22d5ff2c64732737e3d4ed088d6fa9a620f4.

---
+++ Segment1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3==0 && i% 5==0)
+                {
+                    Console.WriteLine("SupportTraining");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Training");
+                }
+                else if (i % 5 == 0)
+                { Console.WriteLine("Support"); }
+                else
+                {
+                     Console.WriteLine(i.ToString());
+                }
+            }
+            Console.Read();
+
+
+
+        }
+    }
+}
